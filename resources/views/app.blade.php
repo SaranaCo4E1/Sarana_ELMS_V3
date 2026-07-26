4a06fa6fba4f9bb7bb7fc1e68fe3209a5f8a2244
Sean Sophearom <phearom@niy.ai>
2026-07-26 15:57:19 +0700

    Use corporate NiyAI favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>{{ config('app.name', 'NiyAI ELMS') }}</title>
-        <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
+        <link rel="icon" type="image/png" href="{{ asset('images/niyai-logo.png') }}">
 
         <!-- Google Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
